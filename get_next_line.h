2071004e802d0e32689e82d4bdc46830eb47ae9c
Naila MBABAZI <nmbabazi@e2r13p13.42.fr>
2020-01-10 09:00:24 +0100

    final

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmbabazi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/08 08:25:16 by nmbabazi          #+#    #+#             */
+/*   Updated: 2020/01/08 08:36:04 by nmbabazi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+int		ft_lenline(char *str);
+int		ft_verifline(char *str);
+int		ft_creatline(char *buf, char **line);
+
+#endif
